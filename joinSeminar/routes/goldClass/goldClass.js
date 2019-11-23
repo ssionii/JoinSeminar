699276ce3ca2f9dde23b4c24a9d84e5af59bad27
ssionii <ssionii353@gmail.com>
2019-11-23 15:28:03 +0900

    model 생성

---
+++ joinSeminar/routes/goldClass/goldClass.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+
+const statusCode = require('../../module/statusCode');
+const responseMessage = require('../../module/responseMessage');
+const authUtil = require('../../module/authUtil');
+
+// hot, new, all 가져오기
+router.get('/:category', (req, res)=>{
+    const category = req.params.category;
+    if(!category){
+        res.status(statusCode.BAD_REQUEST)
+        .send(authUtil.successFalse(responseMessage.NULL_VALUE));
+        return;
+    }
+})
